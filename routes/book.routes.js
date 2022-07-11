31282dc11984071d43ffd075d83ce3551b9c4290
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-07-11 14:53:07 +0200

    (READ) book details page

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -4,7 +4,6 @@ const router = require("express").Router();
 
 
 router.get("/books", (req, res, next) => {
-
   Book.find()
     .then( (booksFromDB) => {
       const data = {
@@ -16,8 +15,21 @@ router.get("/books", (req, res, next) => {
       console.log("Error getting data from DB", error);
       next(error);
     })
-
 });
 
+
+router.get("/books/:bookId", (req, res) => {
+  const bookId = req.params.bookId;
+
+  Book.findById(bookId)
+    .then( (bookDetails) => {
+      res.render("books/book-details", bookDetails);
+    })
+    .catch( (error) => {
+      console.log("Error getting book details from DB", error);
+      next(error);
+    })
+
+})
 
 module.exports = router;
